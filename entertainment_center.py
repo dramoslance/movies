f57274fd6418269296aa2bf3cad10247ec4aa1dd
Deiby Ramos Avila <deiby.ramos@gmail.com>
2015-11-15 21:52:11 -0430

    1. Fix format code; 2. Change to new-style class

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import fresh_tomatoes
 import media
 
-
-
-"""entertainment_center.py: This script allows to create instances of movies an actors from static data.
+"""entertainment_center.py: This script allows to
+create instances of movies an actors from static data.
 
 We have Movies features like:
 
-titles, prices, informations, trailers, posters, storylines, dates, 
+titles, prices, informations, trailers, posters, storylines, dates,
 
 and Actors features like:
 
@@ -54,38 +56,105 @@ posters = [
         "Midnight_in_Paris_Poster.jpg/215px-Midnight_in_Paris_Poster.jpg"),
     "http://www.cineol.net/galeria/carteles/bigtmp_25864.jpg"]
 story_lines = [
-    "In a Louisiana nursing home in 1999, Paul Edgecomb becomes nervous while watching the 1935 film Top Hat. He is with his elderly friend Elaine, who becomes concerned, and Paul tells her that the film reminded him of his past, when he was a prison officer in charge of death row inmates at the Cold Mountain Penitentiary during the summer of 1935.",
-    "With the nation of Panem in a full-scale revolutionary war, Katniss (Jennifer Lawrence) confronts President Snow (Donald Sutherland) in the final showdown. Teamed with a group of her closest friends – including Gale (Liam Hemsworth), Finnick (Sam Claflin), Cressida (Natalie Dormer) and Peeta (Josh Hutcherson) – Katniss goes off on a mission with the District 13 unit as they risk their lives to liberate the citizens of Panem, and stage an assassination attempt on President Snow who has become increasingly obsessed with destroying her. Although rebels - including fellow victors, Johanna (Jena Malone), Haymitch (Woody Harrelson), and Beetee (Jeffrey Wright)- now control most of Panem, the girl on fire must still overcome one last challenge to win President Snow's 'game' - to conquer the Capitol at the risk of losing her friends and loved ones. The mortal traps, enemies, and moral choices that await Katniss will challenge her more than any arena she faced in The Hunger Games as she realizes the stakes are no longer just for survival – they are for the future of Panem.",
-    "By 2154, humans have depleted Earth's natural resources, leading to a severe energy crisis. The Resources Development Administration, RDA for short, mines for a valuable mineral – unobtanium – on Pandora, a densely forested habitable moon orbiting the gas giant Polyphemus in the Alpha Centauri star system.[10] Pandora, whose atmosphere is poisonous to humans, is inhabited by the Na'vi, 10-foot tall (3.0 m), blue-skinned, sapient humanoids[33] who live in harmony with nature and worship a mother goddess called Eywa.",
-    "In a futuristic dystopian Chicago, the society is divided into five factions: Abnegation (the selfless), Amity (the peaceful), Candor (the honest), Dauntless (the brave), and Erudite (the intellectual). The remaining population are the Factionless, who have no status or privilege in this society. When children reach the age of 16, they undergo a serum-induced psychological test which indicates their best-suited faction, then are allowed to choose any faction as their permanent group at the subsequent Choosing Ceremony.",
-    "Gil Pender, a successful but creatively unfulfilled Hollywood screenwriter, and his fiancée, Inez, are in Paris, vacationing with Inez's wealthy, conservative parents. Gil is struggling to finish his first novel, centered on a man who works in a nostalgia shop. Inez dismisses his ambition as a romantic daydream and encourages him to stick with lucrative screenwriting. Gil is considering moving to Paris (which he notes, much to the dismay of his fiancée, is at its most beautiful in the rain). Inez is intent on living in Malibu. By chance, they are joined by Inez's friend Paul who is described as both pedantic and a pseudo-intellectual, and who speaks with great authority but questionable accuracy on the history and art of Paris. Paul contradicts a tour guide at the Rodin Museum, and insists that his knowledge of Rodin's relationships is more accurate than that of the tour guide.[7] Inez admires him; Gil finds him insufferable.",
-    "A lonely businessman temporarily relocates to Maine after a bad breakup. He moves into a rundown cottage, where he meets the beautiful spirit of a woman who lived in the house fifty years before. As they form a friendship, they begin to help each other in ways they never knew were possible, and change both their fortunes forever."
+    "In a Louisiana nursing home in 1999, Paul Edgecomb becomes nervous while"
+    "watching the 1935 film Top Hat. He is with his elderly friend Elaine, who"
+    "becomes concerned, and Paul tells her that the film reminded him of his"
+    "past, when he was a prison officer in charge of death row inmates at the"
+    "Cold Mountain Penitentiary during the summer of 1935.",
+    "With the nation of Panem in a full-scale revolutionary war, Katniss ("
+    "Jennifer Lawrence) confronts President Snow (Donald Sutherland) in the"
+    "final showdown. Teamed with a group of her closest friends – including"
+    "Gale(Liam Hemsworth), Finnick(Sam Claflin), Cressida(Natalie Dormer) and"
+    "Peeta (Josh Hutcherson) – Katniss goes off on a mission with the District"
+    "13 unit as they risk their lives to liberate the citizens of Panem, and"
+    "stage an assassination attempt on President Snow who has become"
+    "increasingly obsessed with destroying her. Although rebels - including"
+    "fellow victors, Johanna (Jena Malone), Haymitch (Woody Harrelson), and"
+    "Beetee (Jeffrey Wright)- now control most of Panem, the girl on fire must"
+    "still overcome one last challenge to win President Snow's 'game' - to"
+    "conquer the Capitol at the risk of losing her friends and loved ones. The"
+    "mortal traps, enemies, and moral choices that await Katniss will"
+    "challenge"
+    "her more than any arena she faced in The Hunger Games as she realizes the"
+    "stakes are no longer just for survival–they are for the future of Panem.",
+    "By 2154, humans have depleted Earth's natural resources, leading to a"
+    "severe energy crisis. The Resources Development Administration, RDA for"
+    "short, mines for a valuable mineral – unobtanium – on Pandora, a densely"
+    "forested habitable moon orbiting the gas giant Polyphemus in the Alpha"
+    "Centauri star system. Pandora, whose atmosphere is poisonous to humans,"
+    "is inhabited by the Na'vi, 10-foot tall (3.0 m), blue-skinned, sapient"
+    "humanoids who live in harmony with nature and worship a mother goddess"
+    "called Eywa.",
+    "In a futuristic dystopian Chicago, the society is divided into five"
+    "factions: Abnegation (the selfless), Amity (the peaceful), Candor (the"
+    "honest), Dauntless (the brave), and Erudite (the intellectual). The"
+    "remaining population are the Factionless, who have no status or privilege"
+    "in this society. When children reach the age of 16, they undergo a serum-"
+    "induced psychological test which indicates their best-suited faction,"
+    "then"
+    "are allowed to choose any faction as their permanent group at the"
+    "subsequent Choosing Ceremony.",
+    "Gil Pender, a successful but creatively unfulfilled Hollywood"
+    "screenwriter, and his fiancée, Inez, are in Paris, vacationing with"
+    "Inez's"
+    "wealthy, conservative parents. Gil is struggling to finish his first"
+    "novel,"
+    "centered on a man who works in a nostalgia shop. Inez dismisses his"
+    "ambition as a romantic daydream and encourages him to stick with"
+    "lucrative"
+    "screenwriting. Gil is considering moving to Paris(which he notes, much to"
+    "the dismay of his fiancée, is at its most beautiful in the rain). Inez is"
+    "intent on living in Malibu. By chance, they are joined by Inez's friend"
+    "Paul who is described as both pedantic and a pseudo-intellectual, and who"
+    "speaks with great authority but questionable accuracy on the history and"
+    "art of Paris. Paul contradicts a tour guide at the Rodin Museum, and"
+    "insists that his knowledge of Rodin's relationships is more accurate than"
+    "that of the tour guide. Inez admires him; Gil finds him insufferable.",
+    "A lonely businessman temporarily relocates to Maine after a bad breakup."
+    "He moves into a rundown cottage, where he meets the beautiful spirit of a"
+    "woman who lived in the house fifty years before. As they form a"
+    "friendship,"
+    "they begin to help each other in ways they never knew were possible, and"
+    "change both their fortunes forever."
 ]
 dates = ["1999", "2015", "2009", "2014", "2011", "2011"]
 
-actors = ["Tom Hanks", "Jennifer Lawrence", "Zoe Saldaña", "Shailene Woodley", "Owen Wilson", "Rob Estes" ]
-actors_genders = ["male", "female", "female", "female", "male", "male" ]
-actors_nationalities = ["USA", "USA", "USA", "USA", "USA", "USA" ]
+actors = [
+    "Tom Hanks",
+    "Jennifer Lawrence",
+    "Zoe Saldaña",
+    "Shailene Woodley",
+    "Owen Wilson",
+    "Rob Estes"]
+actors_genders = ["male", "female", "female", "female", "male", "male"]
+actors_nationalities = ["USA", "USA", "USA", "USA", "USA", "USA"]
 actors_ages = ["59", "25", "37", "23", "46", "46"]
 
 movies = []
 
 
-#Here we create movies and actors instances from previous defined data.
-#Moreover we append movies instances in a list.
+# Here we create movies and actors instances from previous defined data.
+# Moreover we append movies instances in a list.
 for i in range(len(titles)):
     current_movie = media.Movie()
-    current_movie.set_title(titles[i].title())
-    current_movie.set_synopsis_url(informations[i])
-    current_movie.set_story_line((story_lines[i][:500]) if len(story_lines[i]) > 500 else story_lines[i])
-    current_movie.set_price(prices[i])
-    current_movie.set_poster_image(posters[i])
-    current_movie.set_url_trailer(trailers[i])
-    current_movie.set_publishment_date(dates[i])
+    current_movie.title = titles[i].title()
+    current_movie.synopsis_url = informations[i]
+    current_movie.story_line = (
+        (story_lines[i][:500])
+        if len(story_lines[i]) > 500
+        else story_lines[i])
+    current_movie.price = prices[i]
+    current_movie.poster_image_url = posters[i]
+    current_movie.trailer_youtube_url = trailers[i]
+    current_movie.publishment_date = dates[i]
 
-    current_actor = media.Actor(actors[i], actors_genders[i], actors_nationalities[i], actors_ages[i])            
-    current_movie.set_actor(current_actor)
-    movies.append(current_movie)        
+    current_actor = media.Actor(
+        actors[i],
+        actors_genders[i],
+        actors_nationalities[i],
+        actors_ages[i])
+    current_movie.actor = current_actor
+    movies.append(current_movie)
 
-#Passing movies list to render in HTML structure.
+# Passing movies list to render in HTML structure.
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,106 +1,296 @@
 import webbrowser
 
 
-class Movie:       
+class Movie(object):
     """This class represents movies entity, created to storage all information
     related."""
-    
+
     def __init__(self):
         """ Inits the movie instance."""
         self.data = []
-        self.visits = 0        
-        
-    def set_title(self, title):
+        self._visits = 0
+
+    @property
+    def title(self):
+        """Gets the title for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie title.
+
+        Raise:
+            None.
+        """
+        return self._title
+
+    @title.setter
+    def title(self, value):
         """Sets the title for a movie.
 
         Args:
-            title: A string with movie title.
+            value: A string with movie title.
 
         Raise:
             None.
         """
-        self.title = title
+        self._title = value
 
-    def set_price(self, price):
+    @title.deleter
+    def title(self):
+        del self._title
+
+    @property
+    def price(self):
+        """Gets the price for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie price.
+
+        Raise:
+            None.
+        """
+        return self._price
+
+    @price.setter
+    def price(self, value):
         """Sets the price for a movie.
 
         Args:
-            price: A float with movie price.
+            value: A float with movie price.
 
         Raise:
             None.
         """
-        self.price = price
-    
-    def set_story_line(self, story_line):
+        self._price = value
+
+    @price.deleter
+    def price(self):
+        del self._price
+
+    @property
+    def story_line(self):
+        """Gets the story line for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie story line.
+
+        Raise:
+            None.
+        """
+        return self._story_line
+
+    @story_line.setter
+    def story_line(self, value):
         """Sets the story line for a movie.
 
         Args:
-            story_line: A string with movie story line.
-            
+            value: A string with movie story line.
+
         Raise:
             None.
         """
-        self.story_line = story_line
+        self._story_line = value
 
-    def set_poster_image(self, poster_image_url):
+    @story_line.deleter
+    def story_line(self):
+        del self._story_line
+
+    @property
+    def poster_image_url(self):
+        """Gets the poster image url for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie poster image url.
+
+        Raise:
+            None.
+        """
+        return self._poster_image_url
+
+    @poster_image_url.setter
+    def poster_image_url(self, value):
         """Sets the poster image url for a movie.
 
         Args:
-            poster_image_url: A string with movie poster image url.
+            value: A string with movie poster image url.
 
         Raise:
             None.
         """
-        self.poster_image_url = poster_image_url
-    
-    def set_url_trailer(self, trailer_youtube_url):
+        self._poster_image_url = value
+
+    @poster_image_url.deleter
+    def poster_image_url(self):
+        del self._poster_image_url
+
+    @property
+    def trailer_youtube_url(self):
+        """Gets the trailer youtube url for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie trailer youtube url.
+
+        Raise:
+            None.
+        """
+        return self._trailer_youtube_url
+
+    @trailer_youtube_url.setter
+    def trailer_youtube_url(self, value):
         """Sets the trailer youtube url for a movie.
 
         Args:
-            trailer_youtube_url: A string with movie trailer youtube url.
+            value: A string with movie trailer youtube url.
 
         Raise:
             None.
         """
-        self.trailer_youtube_url = trailer_youtube_url
+        self._trailer_youtube_url = value
 
-    def set_synopsis_url(self, synopsis_url):
+    @trailer_youtube_url.deleter
+    def trailer_youtube_url(self):
+        del self._trailer_youtube_url
+
+    @property
+    def synopsis_url(self):
+        """Gets the synopsis url for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie synopsis url.
+
+        Raise:
+            None.
+        """
+        return self._synopsis_url
+
+    @synopsis_url.setter
+    def synopsis_url(self, value):
         """Set url to fully shows the content of movie.
 
         Args:
-            synopsis_url: A string with movie information url.
+            value: A string with movie information url.
 
         Raise:
             None.
         """
-        self.synopsis_url = synopsis_url
+        self._synopsis_url = value
 
-    def set_publishment_date(self, publishment_date):
+    @synopsis_url.deleter
+    def synopsis_url(self):
+        del self._synopsis_url
+
+    @property
+    def publishment_date(self):
+        """Gets the publishment date for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie publishment date.
+
+        Raise:
+            None.
+        """
+        return self._publishment_date
+
+    @publishment_date.setter
+    def publishment_date(self, value):
         """Sets the publishment date for a movie.
 
         Args:
-            date_publish: A float with movie publishment date.
+            value: A float with movie publishment date.
 
         Raise:
             None.
         """
-        self.publishment_date = publishment_date
+        self._publishment_date = value
 
-    def set_visits(self, visits):
+    @publishment_date.deleter
+    def publishment_date(self):
+        del self._publishment_date
+
+    @property
+    def visits(self):
+        """Gets the visits for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie visits.
+
+        Raise:
+            None.
+        """
+        return self._visits
+
+    @visits.setter
+    def visits(self, value):
         """Sets the visits for a movie.
 
         Args:
-            visits: A integer with movie visits.
+            value: A integer with movie visits.
 
         Raise:
             None.
         """
-        self.visits = visits
+        self._visits = value
 
-    def set_actor(self, actor):
-        """ Allows to set actor to the movie"""
-        self.actor = actor;
+    @visits.deleter
+    def visits(self):
+        del self._visits
+
+    @property
+    def actor(self):
+        """Gets the actor for a movie.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Movie actor.
+
+        Raise:
+            None.
+        """
+        return self._actor
+
+    @actor.setter
+    def actor(self, value):
+        """ Allows to set actor to the movie
+
+        Args:
+            value: actor instance
+
+        Returns:
+            None.
+
+        Raises:
+            None.
+        """
+        self._actor = value
+
+    @actor.deleter
+    def actor(self):
+        del self._actor
 
     def show_trailer(self):
         """Shows movie trailer in browser.
@@ -110,68 +300,146 @@ class Movie:
 
         Args:
             None.
-            
+
         Raise:
             None.
         """
-        self.visits = self.visits + 1;
-        webbrowser.open(self.trailer_youtube_url);
-    
+        self.visits = self.visits + 1
+        webbrowser.open(self.trailer_youtube_url)
 
 
-class Actor():
+class Actor(object):
     """Represents actors of the movies"""
 
     def __init__(self):
         self.data = []
 
     def __init__(self, name, gender, nationality, age):
-        self.name = name
-        self.gender = gender
-        self.nationality = nationality
-        self.age = age
+        self._name = name
+        self._gender = gender
+        self._nationality = nationality
+        self._age = age
 
-    def set_name(self, name):
+    @property
+    def name(self):
+        """Gets the actor name.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Actor name.
+
+        Raise:
+            None.
+        """
+        return self._name
+
+    @name.setter
+    def name(self, value):
         """Sets the name for an actor.
 
         Args:
-            name: A string with actor name.
+            value: A string with actor name.
 
         Raise:
             None.
         """
-        self.name = name
+        self._name = value
 
-    def set_gender(self, gender):
+    @name.deleter
+    def name(self):
+        del self._name
+
+    @property
+    def gender(self):
+        """Gets the actor gender.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Actor gender.
+
+        Raise:
+            None.
+        """
+        return self._gender
+
+    @gender.setter
+    def gender(self, value):
         """Sets the gender for an actor.
 
         Args:
-            gender: A string with actor gender.
+            value: A string with actor gender.
 
         Raise:
             None.
         """
-        self.gender = gender
+        self._gender = value
 
-    def set_nationality(self, nationality):
+    @gender.deleter
+    def gender(self):
+        del self._gender
+
+    @property
+    def nationality(self):
+        """Gets the actor nationality.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Actor nationality.
+
+        Raise:
+            None.
+        """
+        return self._nationality
+
+    @nationality.setter
+    def nationality(self, value):
         """Sets the nationality for an actor.
 
         Args:
-            nationality: A string with actor nationality.
+            value: A string with actor nationality.
 
         Raise:
             None.
         """
-        self.nationality = nationality
+        self._nationality = value
 
-    def set_age(self, age):
+    @nationality.deleter
+    def nationality(self):
+        del self._nationality
+
+    @property
+    def age(self):
+        """Gets the actor age.
+
+        Args:
+            self: current instance.
+
+        Returns:
+            Actor age.
+
+        Raise:
+            None.
+        """
+        return self._age
+
+    @age.setter
+    def age(self, value):
         """Sets the age for an actor.
 
         Args:
-            age: A string with actor age.
+            value: A string with actor age.
 
         Raise:
             None.
         """
-        self.age = age
-        
+        self._age = value
+
+    @age.deleter
+    def age(self):
+        del self._age
